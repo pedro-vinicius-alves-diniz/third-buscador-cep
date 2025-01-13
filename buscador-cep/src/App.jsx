ce5ddcb5a88374fb207baf51019b0e9d92268329
Pedro Vinicius Alves Diniz <pedrinhovini318@icloud.com>
2025-01-13 11:51:23 +0100

    update

--- buscador-cep/src/App.jsx
+++ buscador-cep/src/App.jsx
@@ -1,20 +1,29 @@
 import { FiSearch } from "react-icons/fi";
-import "./app.css"
+import "./app.css";
 
-function App(){
-    return(
-        <div className="container">
-            <h1>BUSCADOR CEP</h1>
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">BUSCADOR CEP</h1>
 
-            <div className="containerInput">
-                <input className="inputCep" type="text" placeholder="Digite o CEP"/>
+      <div className="containerInput">
+        <input className="inputCep" type="text" placeholder="Digite o CEP" />
 
-                <button className="buttonSearch">
-                    <FiSearch/>
-                </button>
-            </div>
-        </div>
-    )
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#fff" />
+        </button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: 00000000</h2>
+
+        <span>Rua Teste Algum</span>
+        <span>Complemento</span>
+        <span>Vila</span>
+        <span>Natal/RN</span>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
